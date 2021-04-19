a9b67c6b0cb0327a3418997a845ca4a4b8c602aa
Kyle Shepard <34076343+kshepard379@users.noreply.github.com>
2021-04-18 20:17:25 -0700

    added bmp loading

--- include/loadingFunctions.h
+++ include/loadingFunctions.h
@@ -12,12 +12,12 @@
 //Pull off the first element of sub up to delim
 // Ex: sub |0.877342 0.081279 -0.329742| delim: " "
 // returns |0.877342|
-// changes sub (via reference) to |0.081279 -0.329742|  
+// changes sub (via reference) to |0.081279 -0.329742|
 std::string parseAndClip(std::string& sub, std::string delim);
 
 //Object Loading Information
 //Referenced from https://en.wikibooks.org/wiki/OpenGL_Programming/Modern_OpenGL_Tutorial_Load_OBJ
-// and http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/ 
+// and http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
 // When exporting from blender, keep everything default except:
 //       Make sure to include Normals
 //       Include UVs
@@ -31,3 +31,16 @@ std::string parseAndClip(std::string& sub, std::string delim);
 // normals - index with the above vertices
 // number - Total number of points in vertices (should be vertices.length())
 void load_obj(const char* filename, std::vector<vmath::vec4> &vertices, std::vector<vmath::vec2> &uvs, std::vector<vmath::vec4> &normals, GLuint &number);
+
+//Load bitmap info from file into texture_data
+//this assumes bitmap is the correct size (square and a power of two)
+// input: file -> string to the bitmap file location
+//        texture_data is a pointer that is assigned inside the function
+//                     when done using should be deleted
+//                     points to a chunk of data representing RGBA unsigned ints
+//        tWidth/tHeight -> both hold the size of the data being loaded (should be the same)
+void load_BMP(std::string file, unsigned char* &texture_data, unsigned int &tWidth, unsigned int &tHeight);
+
+//Utility function to convert char (hopefully pulled from a binary file) to unsigned int
+//This is useful for converting header info inside of loadCubeSide
+unsigned int charToUInt(char * loc);
